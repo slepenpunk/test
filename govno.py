a48e04bc13e43d99afc7f2c6ca14a08e90d9acc1
slepenpunk <105781981+slepenpunk@users.noreply.github.com>
2022-10-12 22:39:25 +0300

    qq

--- govno.py
+++ govno.py
@@ -1,64 +1,30 @@
-def ask_y_n(question):
-    ask = None
-    while ask not in ('y', 'n'):
-        ask = input(question).lower()
-    return ask
+def open_file(name):
+    text = open(name, 'r', encoding='utf-8')
+    text = text.read()
+    return text
 
 
-def new_file(name):
-    file = open(name, 'a', encoding='utf=8')
-    file.close()
-
-
-def change_file(name):
-    try:
-        file = open(name, 'r+', encoding='utf=8')
-        change = input('Enter new data: ')
-        file.write(change)
-        file.write('\n')
-        file.close()
-    except FileNotFoundError:
-        print('This file is not found!')
-
-
-def read_file(name):
-    try:
-        read_the_file = open(name, 'r', encoding='utf=8')
-        print(read_the_file.read())
-        read_the_file.close()
-    except FileNotFoundError:
-        print('This file is not found!')
+def total_repeats(name_file):
+    d = {}
+    k = (' ', '.', ',', '...', '!', ':', '""', '-', '…', '—', '?', '')
+    for i in name_file:
+        if i not in d:
+            d[i] = 1
+        elif i in k:
+            pass
+        else:
+            d[i] += 1
+    return d
 
 
 def main():
-    choice = None
-    while choice != '0':
-        print('''
-MESHOK  0 - Exit
-  TI    1 - Create new file
-PIDOR   2 - Read file
-!!!!!!  3 - Change file 
-        ''')
-        choice = input('Enter you variation: ')
-        if choice == '0':
-            break
-        elif choice == '1':
-            enter = input('Enter name for new file(.txt): ')
-            new_file(enter)
-            print('-' * 20)
-        elif choice == '2':
-            file = input('Enter name file(.txt): ')
-            read_file(file)
-            print('-' * 20)
-        elif choice == '3':
-            ask = None
-            while ask != 'n':
-                ask = ask_y_n('Do you want enter new data?(y/n): ')
-                if ask == 'n':
-                    break
-                elif ask == 'y':
-                    file = input('Enter the file to change it: ')
-                    change_file(file)
-                    print('-' * 20)
+    the_file = open_file('file.txt')
+    repeats = total_repeats(the_file)
 
+    for i in repeats:
+        if repeats[i] > 1:
+            print(i, '-', repeats[i])
 main()
+
+
+
